a9b79ff2bc4b0bfdaff445a867c53b7f2467832f
Alice Cadillon <acadillon@gmail.com>
2025-03-04 18:39:03 +0100

    new favicon

--- src/components/Content/Actu.jsx
+++ src/components/Content/Actu.jsx
@@ -30,7 +30,7 @@ const Actu = ({ title, titleEN, body, bodyEN, link, linkText, image }) => {
                 {language === 'fr' ? <h3 className='header-body'>{title}</h3> : <h3 className='header-body'>{titleEN}</h3>}
 
                 {link && (
-                    <a href={link} target='_blanck' className='header-body'>{linkText}</a>
+                    <a href={link} target='_blanck' className='header-body'>{linkText} <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className='arrow-ext'><path d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"/></svg></a>
                 )}
             </div>
 
